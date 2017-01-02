c8e7e89f594d409cc43df7418465c6e700636f22
Karthik Meda <karthmeda@gmail.com>
2017-01-02 17:34:22 -0500

    Files for COTD app after video 13

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,28 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this);
+    //getinitialState
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    //update state
+    const fishes={...this.state.fishes};
+    //add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`]=fish;
+    // set state
+    this.setState({ fishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +34,7 @@ class App extends React.Component {
 
       </div>
       <Order/>
-      <Inventory/>
+      <Inventory addFish={this.addFish} />
       </div>
       )
   }
