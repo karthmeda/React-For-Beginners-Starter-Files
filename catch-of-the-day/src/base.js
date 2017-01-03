1e8dfde85a26ba685383c6399efc3b3522143c3e
Karthik Meda <karthmeda@gmail.com>
2017-01-02 20:18:41 -0500

    Added files for COTD app after video 18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+   apiKey: "AIzaSyDzXE9mklMDHHdXD4u-x2NC_clzOr4dD30",
+    authDomain: "catch-of-the-day-karthik.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-karthik.firebaseio.com"
+});
+
+export default base;
