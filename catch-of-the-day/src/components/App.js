c61856b098fed76e28bcaa2eeb1969d74bae0a00
Karthik Meda <karthmeda@gmail.com>
2017-01-03 00:34:00 -0500

    Added files for COTD app after video 22

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -13,9 +13,11 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.removeFish = this.removeFish.bind(this);
     this.updateFish = this.updateFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.removeFromOrder = this.removeFromOrder.bind(this);
     //getinitialState
     this.state = {
       fishes: {},
@@ -65,6 +67,12 @@ class App extends React.Component {
     this.setState({ fishes })
   }
 
+  removeFish(key){
+    const fishes = {...this.state.fishes};
+    fishes[key] =  null;
+    this.setState({ fishes });
+
+  }
   loadSamples () {
     this.setState({
       fishes: sampleFishes
@@ -81,6 +89,12 @@ class App extends React.Component {
     this.setState({ order });
   }
 
+  removeFromOrder(key) {
+    const order= {...this.state.order};
+    delete order[key];
+    this.setState({ order })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -93,8 +107,13 @@ class App extends React.Component {
         }
       </ul>
       </div>
-      <Order fishes={this.state.fishes} order={this.state.order} params={this.props.params}/>
+      <Order fishes={this.state.fishes}
+      order={this.state.order}
+      params={this.props.params}
+      removeFromOrder={this.removeFromOrder}
+      />
       <Inventory addFish={this.addFish}
+      removeFish={this.removeFish}
       loadSamples={this.loadSamples}
       fishes={this.state.fishes}
       updateFish={this.updateFish}
